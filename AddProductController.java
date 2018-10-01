3b1da10305bdf209e06c6bc634bd7573a82e2b26
Sanduni Nirodha Jayaweera <39158597+Sanduni93@users.noreply.github.com>
2018-10-02 00:48:25 +0530

    Add files via upload
    Here I have added combo box values from the database tables for brand names, category names and supplier name.

--- AddProductController.java
+++ AddProductController.java
@@ -2,7 +2,14 @@
 package main;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -60,10 +67,68 @@ public class AddProductController implements Initializable {
     private Button btn_add_inaddproduct;
     @FXML
     private Button btn_back_in_addproduct;
-
+    
+    ObservableList<String> list1 = FXCollections.observableArrayList();
+    ObservableList<String> list2 = FXCollections.observableArrayList();
+    ObservableList<String> list3 = FXCollections.observableArrayList();
+    private DBConnector db;
+    private Connection conn=null;
+    private PreparedStatement pst = null;
+    ResultSet rs = null;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       
-    }    
+      db = new DBConnector();
     
+      //add supplier's names for combobox
+      try {
+        conn = DriverManager.getConnection("jdbc:mysql://localhost/elecdesk", "root", "");
+        pst=conn.prepareStatement("select*from addsuppliertable");
+        rs = pst.executeQuery();
+        while(rs.next())
+        {
+            list1.add(rs.getString("supname"));
+        }
+        }
+        catch (SQLException e) 
+        {
+            System.out.println(e);
+        }
+        
+         combo_supname_in_addproduct.setItems(list1);
+           //add brand names for combobox
+         try {
+        conn = DriverManager.getConnection("jdbc:mysql://localhost/elecdesk", "root", "");
+        pst=conn.prepareStatement("select*from addbrandstable");
+        rs = pst.executeQuery();
+        while(rs.next())
+        {
+            list2.add(rs.getString("brandname"));
+        }
+        }
+        catch (SQLException e) 
+        {
+            System.out.println(e);
+        }
+        
+         combo_brand_in_addproduct.setItems(list2);
+         
+         //add category names for combobox   
+         
+         try {
+        conn = DriverManager.getConnection("jdbc:mysql://localhost/elecdesk", "root", "");
+        pst=conn.prepareStatement("select*from addcategorytable");
+        rs = pst.executeQuery();
+        while(rs.next())
+        {
+            list3.add(rs.getString("categoryname"));
+        }
+        }
+        catch (SQLException e) 
+        {
+            System.out.println(e);
+        }
+        
+         combo_category_in_addproduct.setItems(list3);
+    
+}
 }
