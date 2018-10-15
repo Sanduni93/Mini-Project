17e7713480f660ae7a0746311f1c71e7228fb1ee
Sanduni Nirodha Jayaweera <39158597+Sanduni93@users.noreply.github.com>
2018-10-16 00:46:24 +0530

    Add files via upload
    Here I have completed update category overview

--- UpdateCategoryController.java
+++ UpdateCategoryController.java
@@ -1,4 +1,3 @@
-
 package main;
 
 import Dialog.AlertDialogInsert;
@@ -9,7 +8,10 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -27,7 +29,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javax.swing.JOptionPane;
-
 
 public class UpdateCategoryController implements Initializable {
 
@@ -68,55 +69,46 @@ public class UpdateCategoryController implements Initializable {
     @FXML
     private Button btn_back_in_updatecatg;
 
-    
     ObservableList<String> list1 = FXCollections.observableArrayList();
     ObservableList<String> list2 = FXCollections.observableArrayList();
     private DBConnector db;
-    Connection conn=null;
-    PreparedStatement pst=null;
-    ResultSet rs = null;
+    Connection conn ;
+    PreparedStatement pst;
+    ResultSet rs;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       db=new DBConnector();
-    //add brand names for combobox
-         try {
-        conn = DriverManager.getConnection("jdbc:mysql://localhost/elecdesk", "root", "");
-        pst=conn.prepareStatement("select*from addbrandstable");
-        rs = pst.executeQuery();
-        while(rs.next())
-        {
-            list1.add(rs.getString("brandname"));
-        }
-        }
-        catch (SQLException e) 
-        {
+        db = new DBConnector();
+       //add brand names to the combobox
+
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/elecdesk", "root", "");
+            pst = conn.prepareStatement("select*from addbrandstable");
+            rs = pst.executeQuery();
+            while (rs.next()) {
+                list1.add(rs.getString("brandname"));
+            }
+        } catch (SQLException e) {
             System.out.println(e);
         }
-        
-         combo_brandname_in_updatecatg.setItems(list1);
-         
-    //add supplier's names for combobox
-      try {
-        conn = DriverManager.getConnection("jdbc:mysql://localhost/elecdesk", "root", "");
-        pst=conn.prepareStatement("select*from addsuppliertable");
-        rs = pst.executeQuery();
-        while(rs.next())
-        {
-            list2.add(rs.getString("supname"));
-        }
-        }
-        catch (SQLException e) 
-        {
+        combo_brandname_in_updatecatg.setItems(list1);
+        //add suppliers names to combobox
+              try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/elecdesk", "root", "");
+            pst = conn.prepareStatement("select*from addsuppliertable");
+            rs = pst.executeQuery();
+            while (rs.next()) {
+                list2.add(rs.getString("supname"));
+            }
+        } catch (SQLException e) {
             System.out.println(e);
         }
-        
-         combo_supname_in_updatecatg.setItems(list2);
-        
-        
-        
+        combo_supname_in_updatecatg.setItems(list2);
+
     }
-    //load category details to fields
+    
+    //load category details to update
+    
      public class Function {
 
         @FXML
@@ -132,84 +124,73 @@ public class UpdateCategoryController implements Initializable {
             return rs;
         }
     }
-   
-      @FXML
-    public void btnGoToUpdateCategoryDetails(ActionEvent event) throws SQLException {
-
-        UpdateCategoryController.Function f = new UpdateCategoryController.Function();
-        ResultSet rs = null;
-        String catgid = "categoryid";
-        String catgname = "categoryname";
-        String brandname = "brandname";
-        String supname = "supname";
-        String date = "date" ;
-        String des = "des";
-       
-
-        rs = f.find(tf_enter_catgid_toupdate.getText());
-
-        try {
-
-            if (rs.next()) {
-                tf_catgid_in_updatecatg.setText(rs.getString("categoryid")); 
-                tf_catgname_in_updatecatg.setText(rs.getString("categoryname"));
-                combo_brandname_in_updatecatg.setValue(rs.getString("brandname"));
-                combo_supname_in_updatecatg.setValue(rs.getString("supname"));
-               
-                tf_des_in_updatecatg.setText(rs.getString("des")); 
-              
-            } else {
-
-                JOptionPane.showMessageDialog(null, "No data for this index");
-
-            }
-
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, ex.getMessage());
-
-        }
-       
-
-    }
-    //update category
      @FXML
-    public void btnUpdateCategory(ActionEvent event) throws SQLException {
-
+     public void btnGoToLoadCatgDetailsToUpdate(ActionEvent event)
+     {
+     
+     UpdateCategoryController.Function f = new UpdateCategoryController.Function();
+     String categoryid = "categoryid";
+     String categoryname = "categoryname";
+     String brandname = "brandname";
+     String supname = "supname";
+     String date = "date";
+     String des = "des";
+     
+     rs = f.find(tf_enter_catgid_toupdate.getText());
+     
+     try{
+      if(rs.next())
+      {
+      tf_catgid_in_updatecatg.setText(rs.getString("categoryid"));
+      tf_catgname_in_updatecatg.setText(rs.getString("categoryname"));
+      combo_brandname_in_updatecatg.setValue(rs.getString("brandname"));
+      combo_supname_in_updatecatg.setValue(rs.getString("supname"));
+      
+      tf_des_in_updatecatg.setText(rs.getString("des"));
+      
+     }else{
+      JOptionPane.showMessageDialog(null, "No data for this categoryid");
+      }   
+     
+     }
+     catch(SQLException ex)
+     {
+     JOptionPane.showMessageDialog(null, ex.getMessage());
+     
+     }
+     }
+     //update category details
+     @FXML
+     public void btnUpdateCategory(ActionEvent event) throws SQLException {
+     
         try {
-
-          String catgid = tf_catgid_in_updatecatg.getText();
-          String catgname = tf_catgname_in_updatecatg.getText();
-          String brandname = combo_brandname_in_updatecatg.getValue();
-          String supname = combo_supname_in_updatecatg.getValue();
-          String date = datepicker_date_in_updatecatg.getEditor().getText();
-          String des = tf_des_in_updatecatg.getText();
-
-            String sql = "update addcategorytable set categoryid='" + catgid + "',categoryname='"+catgname+"',brandname='" + brandname + "',supname = '"+supname+"',date='"+date+"',des='" + des + "'";
-            pst = conn.prepareStatement(sql);
-            pst.execute();
+            String categoryid = tf_catgid_in_updatecatg.getText();
+            String categoryname = tf_catgname_in_updatecatg.getText();
+            String brandname = combo_brandname_in_updatecatg.getSelectionModel().getSelectedItem();
+            String supname = combo_supname_in_updatecatg.getSelectionModel().getSelectedItem();
+            String date = ((TextField)datepicker_date_in_updatecatg.getEditor()).getText();
+            String des=tf_des_in_updatecatg.getText();
             
-            AlertDialogInsert.display("Connection", "Data Updated Successfully");
-
-        } 
-        catch (Exception e) {
-
-            JOptionPane.showMessageDialog(null,"Data Updated Successfully");
-
+            String sql = "update addcategorytable set categoryid='"+categoryid+"',categoryname='"+categoryname+"',"
+                   + "brandname='"+brandname+"',supname='"+supname+"',date='"+date+"',des='"+des+"' where categoryid='"+categoryid+"'";
+           
+            pst = conn.prepareStatement(sql);
+            //pst.execute();
+             AlertDialogInsert.display("Connection", "Data Updated Successfully");
+        } catch (SQLException ex) {
+         Logger.getLogger(UpdateCategoryController.class.getName()).log(Level.SEVERE, null, ex);
+    
         }
-    }
-    
-    @FXML
-    public void btnGoBackToCategoryOverviewInUpdateCatg(ActionEvent event) throws IOException
-    {
-    Parent catgGoBack = FXMLLoader.load(getClass().getResource("CategoryOverview.fxml"));
-    Scene category2= new Scene(catgGoBack);
-    
-    Stage showCategory2 = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    showCategory2.setScene(category2);
-    showCategory2.show();
-    }
+        finally{
+        pst.execute();
+        pst.close();
+        }
+         
+     
+     
+     }
 
 
     
-    
+
 }
